51af8d5b118520a56250cd1eaad07816edc19e04
DaylinMDO09 <daylinmf09@gmail.com>
2025-07-24 00:03:55 -0400

    una pequeña actualización

--- ProyectoFinal-LP3 (Daylin)/obj/Debug/net8.0/ProyectoFinal-LP3 (Daylin).AssemblyInfo.cs
+++ ProyectoFinal-LP3 (Daylin)/obj/Debug/net8.0/ProyectoFinal-LP3 (Daylin).AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ProyectoFinal-LP3 (Daylin)")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b911a5abb52b037ef08b6a2ef5d751d55cff1e84")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0a34eff8ef383451485df5f06034c038c48f123d")]
 [assembly: System.Reflection.AssemblyProductAttribute("ProyectoFinal-LP3 (Daylin)")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ProyectoFinal-LP3 (Daylin)")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
