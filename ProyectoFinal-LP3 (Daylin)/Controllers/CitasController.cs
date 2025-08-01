e8dc3ec584866fa551f062c20770c4bb3c363bbb
DaylinMDO09 <daylinmf09@gmail.com>
2025-07-31 23:55:42 -0400

    Proyecto final 31/7

--- ProyectoFinal-LP3 (Daylin)/Controllers/CitasController.cs
+++ ProyectoFinal-LP3 (Daylin)/Controllers/CitasController.cs
@@ -106,17 +106,114 @@ namespace ProyectoFinal_LP3__Daylin_.Controllers
             //return View(cita);
         }
 
+        [HttpGet]
+        public IActionResult Editar(int idc)
+        {
+            ViewBag.Pacientes = _context.Pacientes
+            .Select(p => new SelectListItem
+            {
+                Value = p.IdPaciente.ToString(),
+                Text = p.nombrePaciente + " - " + p.cedulaPaciente
+            }).ToList();
+
+            ViewBag.Dentistas = _context.Dentistas
+            .Select(d => new SelectListItem
+            {
+                Value = d.idDentista.ToString(),
+                Text = d.nombreDentista
+            }).ToList();
+
+            ViewBag.Motivos = _context.Motivos
+            .Select(m => new SelectListItem
+            {
+                Value = m.idMotivo.ToString(),
+                Text = m.descripcionMotivo
+            }).ToList();
+            var cita = _context.Citas.FirstOrDefault(c => c.idCita == idc);
+            if (cita == null)
+            {
+                TempData["Mensaje"] = "No existe la cita indicada.";
+                return RedirectToAction("Lista");
+            }
+            return View(cita); 
+        }
+
+        [HttpPost]
+        public IActionResult Editar(CitaViewModel cita)
+        {
+            ViewBag.Pacientes = _context.Pacientes
+            .Select(p => new SelectListItem
+            {
+                Value = p.IdPaciente.ToString(),
+                Text = p.nombrePaciente + " - " + p.cedulaPaciente
+            }).ToList();
+
+            ViewBag.Dentistas = _context.Dentistas
+            .Select(d => new SelectListItem
+            {
+                Value = d.idDentista.ToString(),
+                Text = d.nombreDentista
+            }).ToList();
+
+            ViewBag.Motivos = _context.Motivos
+            .Select(m => new SelectListItem
+            {
+                Value = m.idMotivo.ToString(),
+                Text = m.descripcionMotivo
+            }).ToList();
+
+            if (cita.fechaCita.Date < DateTime.Now.Date)
+            {
+                TempData["Mensaje"] = "No es posible editar la fecha de esta agenda porque la fecha no es correcta. Por favor verifique que sea una fecha a futuro.";
+                return View("Editar", cita);
+            }
+
+            DateTime inicioCita = cita.fechaCita.Add(cita.horaCita);
+            DateTime finCita = inicioCita.AddMinutes(cita.duracionCita);
+
+            var citarepetida = _context.Citas.Where(c => c.idDentista == cita.idDentista
+            && c.fechaCita == cita.fechaCita.Date).AsEnumerable().Any(c => inicioCita < c.fechaCita.Add
+            (c.horaCita).AddMinutes((double)c.duracionCita) && finCita > c.fechaCita.Add(c.horaCita) && c.idCita == cita.idCita);
 
 
+            if (citarepetida)
+            {
+                TempData["Mensaje"] = "El dentista tiene una cita en ese horario. Por favor indicar otro.";
+            }
+            else if (!ModelState.IsValid)
+            {
+                var citaActual = _context.Citas.FirstOrDefault(c => c.idCita == cita.idCita);
+                if (citaActual == null)
+                {
+                    TempData["Mensaje"] = "La cita seleccionada no  ha podido ser modificada.";
+                    return RedirectToAction("Lista");
+                }
 
+                citaActual.fechaCita = cita.fechaCita;
+                citaActual.horaCita = cita.horaCita;
+                citaActual.duracionCita = cita.duracionCita;
+                citaActual.Motivo = cita.Motivo;
 
+                _context.Citas.Update(citaActual);
+                _context.SaveChanges();
+                TempData["Mensaje"] = "Cita modificada correctamente.";
+                return RedirectToAction("Lista");
+            }
+            return View(cita);
+        }
 
-
-
-
-
-
-
+        public IActionResult Eliminar(int idc)
+        {
+            var cita = _context.Citas.FirstOrDefault(c => c.idCita == idc);
+            if (cita != null)
+            {
+                _context.Citas.Remove(cita);
+                _context.SaveChanges();
+                TempData["Mensaje"] = "La cita ha sido cancelada y eliminada con exito.";
+                return RedirectToAction("Lista");
+            }
+            return View(cita);
+        }
 
         public ActionResult ExportarCSV()
         {
--- ProyectoFinal-LP3 (Daylin)/obj/Debug/net8.0/ProyectoFinal-LP3 (Daylin).AssemblyInfo.cs
+++ ProyectoFinal-LP3 (Daylin)/obj/Debug/net8.0/ProyectoFinal-LP3 (Daylin).AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ProyectoFinal-LP3 (Daylin)")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7997f0a57d8ef9e789a8cb9fd054ee0d30461ae1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1f7d2900cde388cbd456caedb0017d7550a4eb63")]
 [assembly: System.Reflection.AssemblyProductAttribute("ProyectoFinal-LP3 (Daylin)")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ProyectoFinal-LP3 (Daylin)")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
