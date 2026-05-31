90062d9419f1b998eaf1ebda79daf5896872555c
Daylin Monte De Oca <daylinmf09@gmail.com>
2026-05-31 19:43:13 -0400

    31-05

--- ProyectoFinal-LP3 (Daylin)/obj/Debug/net8.0/ProyectoFinal-LP3 (Daylin).AssemblyInfo.cs
+++ ProyectoFinal-LP3 (Daylin)/obj/Debug/net8.0/ProyectoFinal-LP3 (Daylin).AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ProyectoFinal-LP3 (Daylin)")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7d8481dc0924f5f09313f1dd7e489e8bf248be60")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7d2410f9b98a8aca3f974f75a14090137457e16a")]
 [assembly: System.Reflection.AssemblyProductAttribute("ProyectoFinal-LP3 (Daylin)")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ProyectoFinal-LP3 (Daylin)")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
